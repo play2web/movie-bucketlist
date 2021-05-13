752eb46678db6e19f8f9cd836f0a360e0b7f5a32
elenagiura <elenagiura94@gmail.com>
2021-05-13 17:59:49 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Header} from './components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state={
+      movieList:[],
+    }
+  }
+
+  toWatch = (movie) => {
+    //This part will be different affter connecting with firebase.
+    console.log(movie)
+    this.setState= {movieList:this.state.movieList.push(movie)}
+  }
+
+  render () {
+    return (
+      <Header toWatch={this.toWatch} movieList={this.state.movieList}/>
+    )
+  }
 }
 
 export default App;
